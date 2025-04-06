0baa575a537c67c38bf71391af9219f9996011ba
jmk1589@gmail.com <jmk1589@gmail.com>
2025-04-06 13:14:17 +0000

    climate silver layer timestamps + gold layer for ML

--- src/etl/climate/clean.py
+++ src/etl/climate/clean.py
@@ -1,4 +1,4 @@
-from pyspark.sql.functions import avg, to_date, current_timestamp
+from pyspark.sql.functions import avg, to_date, current_timestamp, to_timestamp
 
 def dmi_climate_clean_and_transform(df):
     """
@@ -7,6 +7,9 @@ def dmi_climate_clean_and_transform(df):
     
     df = df.groupBy('observed','stationId','ingestion_timestamp').pivot('parameterId').agg(avg('value'))
     df = df.withColumnRenamed('observed', 'observed_timestamp')
+    df = df.withColumnRenamed('stationId', 'station_id')
+    df = df.withColumn("observed_timestamp", to_timestamp("observed_timestamp"))
+    df = df.withColumn("ingestion_timestamp", to_timestamp("ingestion_timestamp"))
     df = df.withColumn("observed_date", to_date("observed_timestamp"))
     df = df.withColumn('transformation_timestamp', current_timestamp())
     return df
